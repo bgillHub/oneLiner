a74ae3b5115fea8124c29993d6696bbc6d2a57d2
brian <brianrobgill@gmail.com>
2015-11-26 23:37:02 -0500

    Specified Newsfeeds

--- client/script.js
+++ client/script.js
@@ -1,15 +1,16 @@
 JavaTopics = new Mongo.Collection('JavaTopics');
 Template.topicForm.events({
   'click #submit-btn': function(event, template){
-    template.find()
     let topicName = template.find('#topicName').value;
     let topicDifficulty = template.find('#topicDifficulty').value;
     let tags = template.find('#tags').value;
+    let description = template.find('#description').value;
 
     let topic = {
       topicName: topicName,
       topicDifficulty: topicDifficulty,
       tags: tags,
+      description: description
       }
     JavaTopics.insert(topic);
     alert("New Entry: " + topic.topicName + ", " + topic.topicDifficulty);
